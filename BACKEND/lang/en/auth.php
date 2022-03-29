24ab8d53643a2ba85a788957df963357b8c3f2eb
zozzerrr <58724696+zozzerrr@users.noreply.github.com>
2022-03-29 02:45:07 +0200

    Magyarítás

--- BACKEND/lang/en/auth.php
+++ BACKEND/lang/en/auth.php
@@ -12,9 +12,11 @@ return [
     | these language lines according to your application's requirements.
     |
     */
-
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    //'failed' => 'These credentials do not match our records.',
+    'failed' => 'Helytelen bejelentkezési adatokat adtál meg.',
+    //'password' => 'The provided password is incorrect.',
+    'password' => 'A megadott jelszó helytelen.',
+    //'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'throttle' => 'Túl sok bejelentkezési próbálkozás. Kérjük próbálja újra másodperceken másodperceken belül.',
 
 ];
