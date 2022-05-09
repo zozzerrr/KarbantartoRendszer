e3ca71f5d64be4085e87cdbb781b8722f75ea672
nymnY <hege.tamas7@gmail.com>
2022-05-09 23:27:48 +0200

    Lekérdezés
    Szakember feladatainak lekérdezése az adott napra

--- BACKEND/app/Http/Controllers/FeladatController.php
+++ BACKEND/app/Http/Controllers/FeladatController.php
@@ -4,19 +4,34 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Feladat;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class FeladatController extends Controller
 {
      public function index()
      {
-        $feladatok = Feladat::all();
-
+        $feladatok = DB::table('feladat')
+            ->join('karbantartas', 'karbantartas.id', '=', 'feladat.karbantartasid')
+            ->join('eszkoz', 'eszkoz.id', '=', 'karbantartas.eszkozid')
+            ->join('kategoria', 'kategoria.id', '=', 'eszkoz.kategoriaid')
+            ->select('eszkoz.nev','eszkoz.elhelyezkedes','feladat.idopont','karbantartas.sulyossag','karbantartas.allapot','karbantartas.leiras','kategoria.karbantartasInstrukcio','kategoria.normaido')
+            ->where('feladat.szakemberid','=', Auth::user()->id)
+            ->where(DB::raw('DATE(feladat.idopont)') ,'=',DB::raw('CURDATE()'))
+            ->whereIn('karbantartas.allapot', ["Ütemezve","Megkezdve"])
+            ->Where(function ($query) {
+                $query->whereNull("feladat.elfogadtaE");
+                $query->orWhere("feladat.elfogadtaE", "=",1);
+            })
+            ->orderByDesc('karbantartas.allapot')
+            ->orderByDesc('karbantartas.sulyossag')
+            ->get();
 
      }
 
       public function store($request)
       {
-
+            
       }
 
 }
